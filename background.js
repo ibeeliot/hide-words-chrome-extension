3207f9c3ed73c0a6add6485b79dfd97ade293804
Lance Jeffers <lancej1022@gmail.com>
2020-01-29 20:32:49 -0800

    selects p and a tags and successfully removes them

--- background.js
+++ background.js
@@ -25,12 +25,34 @@ chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
 //     alert('hey');
 // }
 
-const elementsOnPage = document.querySelectorAll('p');
-elementsWithMatchingText = Array.from(elementsOnPage).filter(el => el.textContent.includes('You'));
-elementsWithMatchingText.forEach(element => {
+/////////////
+// Make sure finding elements is done case-insensitively
+/////////////
+
+// this will grab all paragraph tags on the page and stored it in an elements array
+const pOnPage = Array.from(document.querySelectorAll('p'));
+// this will search our pOnPage for matching textContent
+const psWithMatchingText = pOnPage.filter(el => el.textContent.includes('You'));
+// delete each paragraph that matched
+psWithMatchingText.forEach(element => {
     element.remove();
 });
-console.log(elementsWithMatchingText);
+
+const aOnPage = Array.from(document.querySelectorAll('a'));
+const asWithMatchingText = aOnPage.filter(el => el.textContent.includes('New'));
+// delete each paragraph that matched
+asWithMatchingText.forEach(element => {
+    element.remove();
+});
+
+// grabs spans on the page
+const spanOnPage = Array.from(document.querySelectorAll('span'));
+// COME BACK TO THIS AND MAYBE SELECT THE PARENT ELEMENT??
+const spanWithMatchingText = spanOnPage.filter(el => el.innerText.includes('You'));
+spanWithMatchingText.forEach(element => {
+    // REFACTOR THIS BECAUSE THE .REMOVE METHOD DOESN'T WORK ON SPAN
+    element.remove();
+});
 
 // var pTags = document.getElementsByTagName("p");
 // var searchText = "You";
