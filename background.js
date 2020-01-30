6f803910863195843e2864f255327c27d5d8ab8b
Lance Jeffers <lancej1022@gmail.com>
2020-01-29 21:31:54 -0800

    being safe

--- background.js
+++ background.js
@@ -28,39 +28,40 @@ chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
 /////////////
 // Make sure finding elements is done case-insensitively
 /////////////
+const deleteStuff = () => {
+    // this will grab all paragraph tags on the page and stored it in an elements array
+    const pOnPage = Array.from(document.querySelectorAll('p'));
+    // this will search our pOnPage for matching textContent
+    const psWithMatchingText = pOnPage.filter(el => el.textContent.includes('You'));
+    // delete each paragraph that matched
+    psWithMatchingText.forEach(element => {
+        element.remove();
+    });
 
-// this will grab all paragraph tags on the page and stored it in an elements array
-const pOnPage = Array.from(document.querySelectorAll('p'));
-// this will search our pOnPage for matching textContent
-const psWithMatchingText = pOnPage.filter(el => el.textContent.includes('You'));
-// delete each paragraph that matched
-psWithMatchingText.forEach(element => {
-    element.remove();
-});
+    const aOnPage = Array.from(document.querySelectorAll('a'));
+    const asWithMatchingText = aOnPage.filter(el => el.textContent.includes('New'));
+    // delete each paragraph that matched
+    asWithMatchingText.forEach(element => {
+        element.remove();
+    });
 
-const aOnPage = Array.from(document.querySelectorAll('a'));
-const asWithMatchingText = aOnPage.filter(el => el.textContent.includes('New'));
-// delete each paragraph that matched
-asWithMatchingText.forEach(element => {
-    element.remove();
-});
+    // // grabs spans on the page
+    // const spanOnPage = Array.from(document.querySelectorAll('span'));
+    // // COME BACK TO THIS AND MAYBE SELECT THE PARENT ELEMENT??
+    // const spanWithMatchingText = spanOnPage.filter(el => el.innerText.includes('You'));
+    // spanWithMatchingText.forEach(element => {
+    //     // REFACTOR THIS BECAUSE THE .REMOVE METHOD DOESN'T WORK ON SPAN
+    //     element.remove();
+    // });
 
-// grabs spans on the page
-const spanOnPage = Array.from(document.querySelectorAll('span'));
-// COME BACK TO THIS AND MAYBE SELECT THE PARENT ELEMENT??
-const spanWithMatchingText = spanOnPage.filter(el => el.innerText.includes('You'));
-spanWithMatchingText.forEach(element => {
-    // REFACTOR THIS BECAUSE THE .REMOVE METHOD DOESN'T WORK ON SPAN
-    element.remove();
-});
+    // var pTags = document.getElementsByTagName("p");
+    // var searchText = "You";
+    // var found;
 
-// var pTags = document.getElementsByTagName("p");
-// var searchText = "You";
-// var found;
-
-// for (var i = 0; i < pTags.length; i++) {
-//   if (pTags[i].textContent == searchText) {
-//     found = pTags[i];
-//     break;
-//   }
-// }
+    // for (var i = 0; i < pTags.length; i++) {
+    //   if (pTags[i].textContent == searchText) {
+    //     found = pTags[i];
+    //     break;
+    //   }
+    // }
+};
